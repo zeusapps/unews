6f24216dd56a33d3a58194d957661c689a835de3
Eugen Zagvozdkin <e.zagvozdkin@gmail.com>
2017-01-07 11:58:47 +0200

    added naive impl for articles repo

---
+++ Source/ZeusApps.News/ZeusApps.News.Server/src/ZeusApps.News.Repository.Inmemory/ArticleRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ZeusApps.News.Models;
+using ZeusApps.News.Repositories;
+
+namespace ZeusApps.News.Repository.Inmemory
+{
+    public class ArticleRepository: IArticleRepository
+    {
+        private readonly List<Article> _articles = new List<Article>();
+
+
+        public Task<Article[]> GetArticles(string sourceId, int count, int offset)
+        {
+            var articles = _articles
+                .Where(x => x.SourceId == sourceId)
+                .Skip(offset)
+                .Take(count)
+                .ToArray();
+
+            return Task.FromResult(articles);
+        }
+
+        public Task<Article> GetArticle(string id)
+        {
+            var article = _articles
+                .FirstOrDefault(x => x.Id == id);
+
+            return Task.FromResult(article);
+        }
+
+        public Task<bool> AddAdticle(Article article)
+        {
+            article.Id = Guid.NewGuid().ToString();
+            _articles.Add(article);
+
+            return Task.FromResult(true);
+        }
+
+        public Task<bool> DeleteArticle(string id)
+        {
+            var article = _articles.FirstOrDefault(x => x.Id == id);
+            if (article == null)
+            {
+                return Task.FromResult(false);
+            }
+
+            _articles.Remove(article);
+            return Task.FromResult(true);
+        }
+    }
+}
