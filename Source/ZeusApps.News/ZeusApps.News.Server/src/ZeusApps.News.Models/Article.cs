a151352d2b1c28989160fed2b58ca3975cddd723
Eugen Zagvozdkin <e.zagvozdkin@gmail.com>
2017-01-07 09:48:21 +0200

    added models proj

---
+++ Source/ZeusApps.News/ZeusApps.News.Server/src/ZeusApps.News.Models/Article.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ZeusApps.News.Models
+{
+    public class Article
+    {
+        public string Id { get; set; }
+
+        public string SourceId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Html { get; set; }
+
+        public string OriginalHtml { get; set; }
+
+        public DateTime Published { get; set; }
+
+        public DateTime Downloaded { get; set; }
+
+        public bool IsClean { get; set; }
+
+        public int Upvote { get; set; }
+
+        public int Downvote { get; set; }
+    }
+}
---
+++ Source/ZeusApps.News/ZeusApps.News.Server/src/ZeusApps.News.Models/Source.cs
@@ -0,0 +1,17 @@
+﻿namespace ZeusApps.News.Models
+{
+    public class Source
+    {
+        public string Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Encoding { get; set; }
+
+        public string BaseUrl { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string Key { get; set; }
+    }
+}
