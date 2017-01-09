b7f11bb1c9ae5c11d0c299e4cd229e28571a8581
Eugen Zagvozdkin <e.zagvozdkin@gmail.com>
2017-01-09 22:22:00 +0200

    addded parser interface

---
+++ Source/ZeusApps.News/ZeusApps.News.Server/src/ZeusApps.News.Parser/Extensions/HtmlExtensions.cs
@@ -0,0 +1,40 @@
+﻿using HtmlAgilityPack;
+
+namespace ZeusApps.News.Parser.Extensions
+{
+    public static class HtmlExtensions
+    {
+        public static HtmlNode Remove(this HtmlNode node, string xpath)
+        {
+            var items = node.SelectNodes(xpath);
+            if (items == null)
+            {
+                return node;
+            }
+
+            foreach (var item in items)
+            {
+                item.Remove();
+            }
+
+            return node;
+        }
+
+        public static HtmlNode RemoveStyles(this HtmlNode node)
+        {
+            var items = node.SelectNodes("//*[@style]");
+
+            if (items == null)
+            {
+                return node;
+            }
+
+            foreach (var item in items)
+            {
+                item.Attributes["style"].Remove();
+            }
+
+            return node;
+        }
+    }
+}
