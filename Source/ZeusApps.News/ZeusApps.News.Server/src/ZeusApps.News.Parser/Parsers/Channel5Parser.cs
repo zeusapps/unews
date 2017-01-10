0b39ab40cdcb2c3cf8ec695b75f479d9b31c02ba
Eugen Zagvozdkin <e.zagvozdkin@gmail.com>
2017-01-10 20:19:35 +0200

    implemented chaneel5 parser

--- Source/ZeusApps.News/ZeusApps.News.Server/src/ZeusApps.News.Parser/Parsers/Channel5Parser.cs
+++ Source/ZeusApps.News/ZeusApps.News.Server/src/ZeusApps.News.Parser/Parsers/Channel5Parser.cs
@@ -1,14 +1,19 @@
-﻿using System.Threading.Tasks;
-using ZeusApps.News.Models;
-using ZeusApps.News.Parser.Infrastructure;
+﻿using ZeusApps.News.Parser.Extensions;
+using ZeusApps.News.Repositories;
 
 namespace ZeusApps.News.Parser.Parsers
 {
-    public class Channel5Parser : IParser
+    public class Channel5Parser : ParserBase
     {
-        public Task<Article[]> Parse(Source source)
+        public Channel5Parser(IArticleRepository repository) : base(repository)
         {
-            return null;
+        }
+
+        protected override string ParseHtml(string html)
+        {
+            return GetDocumentNode(html, "//div[@class='article-content']")
+                .UpdateSource(Source.BaseUrl)
+                .InnerHtml;
         }
     }
 }
