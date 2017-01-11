bcec62bfccd56dca40396cdfd26e3801f2dbde1d
Eugen <e.zagvozdkin@gmail.com>
2017-01-11 15:52:22 +0200

    added parser fasade

---
+++ Source/ZeusApps.News/ZeusApps.News.Server/src/ZeusApps.News.Parser/Parsers/ParserImpl.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using ZeusApps.News.Models;
+using ZeusApps.News.Parser.Infrastructure;
+using ZeusApps.News.Repositories;
+
+namespace ZeusApps.News.Parser.Parsers
+{
+    public class ParserImpl : IParser
+    {
+        private readonly IArticleRepository _repository;
+
+        public ParserImpl(IArticleRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<Article[]> Parse(Source source)
+        {
+            IParser parser;
+
+            switch (source.Key)
+            {
+                case "channel5":
+                    parser = new Channel5Parser(_repository);
+                    break;
+                case "korr":
+                    parser = new KorrParser(_repository);
+                    break;
+                default:
+                    throw new NotImplementedException($"Not implemented processor for {source.Key}");
+            }
+
+            return parser.Parse(source);
+        }
+    }
+}
