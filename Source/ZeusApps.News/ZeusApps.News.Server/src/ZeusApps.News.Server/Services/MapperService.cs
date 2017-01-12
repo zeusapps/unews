bf9d41bf600d68e31e370d8e1c50fdc8caeca4fc
Eugen <e.zagvozdkin@gmail.com>
2017-01-12 11:02:53 +0200

    added mapper service

---
+++ Source/ZeusApps.News/ZeusApps.News.Server/src/ZeusApps.News.Server/Services/MapperService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using AutoMapper;
+using ZeusApps.News.Models;
+using ZeusApps.News.Server.DTOs;
+
+namespace ZeusApps.News.Server.Services
+{
+    public interface IMapperService
+    {
+        TDest Map<TDest>(object source);
+
+        IEnumerable<T> Map<T>(IEnumerable source);
+    }
+    public class MapperService : IMapperService
+    {
+        public MapperService()
+        {
+            Mapper
+                .Initialize(config =>
+                {
+                    config.CreateMap<Source, SourceDto>();
+                });
+        }
+
+        public T Map<T>(object source)
+        {
+            return Mapper.Map<T>(source);
+        }
+
+        public IEnumerable<T> Map<T>(IEnumerable source)
+        {
+            return from object item in source select Map<T>(item);
+        }
+    }
+}
