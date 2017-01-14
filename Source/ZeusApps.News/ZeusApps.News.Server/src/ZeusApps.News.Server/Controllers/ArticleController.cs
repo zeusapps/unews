65cd7d233d4f57fcb820010e7718046ee795d1ca
Eugen Zagvozdkin <e.zagvozdkin@gmail.com>
2017-01-14 22:44:26 +0200

    added logging for article controller, added pagination for atricles

--- Source/ZeusApps.News/ZeusApps.News.Server/src/ZeusApps.News.Server/Controllers/ArticleController.cs
+++ Source/ZeusApps.News/ZeusApps.News.Server/src/ZeusApps.News.Server/Controllers/ArticleController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using ZeusApps.News.Repositories;
 using ZeusApps.News.Server.DTOs;
 using ZeusApps.News.Server.Services;
@@ -9,26 +10,31 @@ namespace ZeusApps.News.Server.Controllers
     [Route("api/[controller]")]
     public class ArticleController : Controller
     {
+        private readonly ILogger<ArticleController> _logger;
         private readonly IArticleRepository _repository;
         private readonly IMapperService _mapperService;
 
         public ArticleController(
+            ILoggerFactory loggerFactory,
             IArticleRepository repository,
             IMapperService mapperService)
         {
+            _logger = loggerFactory.CreateLogger<ArticleController>();
             _repository = repository;
             _mapperService = mapperService;
         }
 
         [HttpGet("{sourceId}")]
-        public async Task<IActionResult> Get(string sourceId)
+        public async Task<IActionResult> Get(string sourceId, int count = 20, int offset = 0)
         {
             if (string.IsNullOrEmpty(sourceId))
             {
+                _logger.LogInformation("sourceId is empty");
                 return BadRequest("sourceId couldn't be null or empty");
             }
 
-            var articles = await _repository.GetArticle(sourceId);
+            var articles = await _repository.GetArticles(sourceId, count, offset);
+            _logger.LogInformation($"Articles: {articles.Length}");
             return Ok(_mapperService.Map<ArticleDto>(articles));
         }
 
@@ -38,9 +44,11 @@ namespace ZeusApps.News.Server.Controllers
             var article = await _repository.GetArticle(articleId);
             if (article == null)
             {
+                _logger.LogInformation("Article not found");
                 return NotFound();
             }
 
+            _logger.LogInformation("Article found", article);
             return Ok(_mapperService.Map<ArticleVoteDto>(article));
         }
 
@@ -51,20 +59,26 @@ namespace ZeusApps.News.Server.Controllers
 
             if (result)
             {
+                _logger.LogInformation($"Article {articleId} upvoted");
                 return Accepted();
             }
-            return BadRequest();
+
+            _logger.LogInformation($"Article {articleId} not found");
+            return NotFound();
         }
 
         [HttpPost("{articleId}/downvote")]
         public async Task<IActionResult> Downvote(string articleId)
         {
             var result = await _repository.Downvote(articleId);
-
+            
             if (result)
             {
+                _logger.LogInformation($"Article {articleId} downvoted");
                 return Accepted();
             }
+
+            _logger.LogInformation($"Article {articleId} not found");
             return BadRequest();
         }
     }
