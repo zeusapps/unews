de240cbbca8416d2def914fc6c2cfc437a41b51e
Eugen Zagvozdkin <e.zagvozdkin@gmail.com>
2017-01-22 21:23:49 +0200

    added constants

---
+++ Source/ZeusApps.News/ZeusApps.News.Clients/Android/app/src/main/java/ua/in/zeusapps/ukrainenews/common/Constants.java
@@ -0,0 +1,10 @@
+package ua.in.zeusapps.ukrainenews.common;
+
+public class Constants {
+
+    public static final String REMOTE_URL = "http://10.0.3.2:54700";
+
+    private Constants(){
+
+    }
+}
