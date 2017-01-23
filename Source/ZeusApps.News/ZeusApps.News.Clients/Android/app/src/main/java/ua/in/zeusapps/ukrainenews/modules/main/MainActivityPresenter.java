916446521631ce8447bc07f33b10657fa898531e
Eugen Zagvozdkin <e.zagvozdkin@gmail.com>
2017-01-23 21:47:05 +0200

    updated presenter

--- Source/ZeusApps.News/ZeusApps.News.Clients/Android/app/src/main/java/ua/in/zeusapps/ukrainenews/modules/main/MainActivityPresenter.java
+++ Source/ZeusApps.News/ZeusApps.News.Clients/Android/app/src/main/java/ua/in/zeusapps/ukrainenews/modules/main/MainActivityPresenter.java
@@ -1,6 +1,11 @@
 package ua.in.zeusapps.ukrainenews.modules.main;
 
 
+import java.util.List;
+
+import rx.Subscriber;
+import ua.in.zeusapps.ukrainenews.models.Source;
+
 public class MainActivityPresenter implements MainActivityMVP.Presenter {
 
     private MainActivityMVP.View _view;
@@ -13,5 +18,22 @@ public class MainActivityPresenter implements MainActivityMVP.Presenter {
     @Override
     public void setView(MainActivityMVP.View view) {
         _view = view;
+
+        _model.getSources().subscribe(new Subscriber<List<Source>>() {
+            @Override
+            public void onCompleted() {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onNext(List<Source> sources) {
+                _view.updateSources(sources);
+            }
+        });
     }
 }
