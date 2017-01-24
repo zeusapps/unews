2b4bd1e5a0444fa87147f3333c38fb73726d2484
Eugen <e.zagvozdkin@gmail.com>
2017-01-24 17:40:46 +0200

    added more acurate type of app

--- Source/ZeusApps.News/ZeusApps.News.Clients/Android/app/src/main/java/ua/in/zeusapps/ukrainenews/modules/ApplicationModule.java
+++ Source/ZeusApps.News/ZeusApps.News.Clients/Android/app/src/main/java/ua/in/zeusapps/ukrainenews/modules/ApplicationModule.java
@@ -1,19 +1,19 @@
 package ua.in.zeusapps.ukrainenews.modules;
 
-import android.app.Application;
 import android.content.Context;
 
 import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
+import ua.in.zeusapps.ukrainenews.common.App;
 
 @Module
 public class ApplicationModule {
 
-    private Application application;
+    private App application;
 
-    public ApplicationModule(Application application) {
+    public ApplicationModule(App application) {
         this.application = application;
     }
 
