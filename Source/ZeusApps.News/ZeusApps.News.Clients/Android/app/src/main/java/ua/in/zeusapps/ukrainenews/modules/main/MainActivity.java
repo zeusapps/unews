159080231d68c140fe788668325ff8ed34b4ae54
Eugen <e.zagvozdkin@gmail.com>
2017-01-25 13:59:59 +0200

    removed source data from main activity

--- Source/ZeusApps.News/ZeusApps.News.Clients/Android/app/src/main/java/ua/in/zeusapps/ukrainenews/modules/main/MainActivity.java
+++ Source/ZeusApps.News/ZeusApps.News.Clients/Android/app/src/main/java/ua/in/zeusapps/ukrainenews/modules/main/MainActivity.java
@@ -3,29 +3,17 @@ package ua.in.zeusapps.ukrainenews.modules.main;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.View;
-
-import java.util.List;
 
 import javax.inject.Inject;
 
-import butterknife.BindView;
 import ua.in.zeusapps.ukrainenews.R;
-import ua.in.zeusapps.ukrainenews.adapter.SourceAdapter;
 import ua.in.zeusapps.ukrainenews.common.BaseActivity;
 import ua.in.zeusapps.ukrainenews.common.BaseMVP;
 import ua.in.zeusapps.ukrainenews.components.ApplicationComponent;
-import ua.in.zeusapps.ukrainenews.models.Source;
 import ua.in.zeusapps.ukrainenews.modules.source.SourceFragment;
 
 public class MainActivity extends BaseActivity implements MainActivityMVP.View {
 
-    private SourceAdapter _adapter;
-
-    @BindView(R.id.activity_main_sourcesRecyclerView)
-    RecyclerView recyclerView;
     @Inject
     MainActivityMVP.Presenter presenter;
 
@@ -48,18 +36,9 @@ public class MainActivity extends BaseActivity implements MainActivityMVP.View {
 
     @Override
     protected void onCreateOverride(@Nullable Bundle savedInstanceState) {
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        _adapter = new SourceAdapter(this);
-        recyclerView.setAdapter(_adapter);
-
         getSupportFragmentManager()
                 .beginTransaction()
                 .add(R.id.activity_main_sourceFragmentPlaceholder, new SourceFragment())
                 .commit();
-    }
-
-    @Override
-    public void updateSources(List<Source> sources) {
-        _adapter.update(sources);
     }
 }
--- Source/ZeusApps.News/ZeusApps.News.Clients/Android/app/src/main/java/ua/in/zeusapps/ukrainenews/modules/main/MainActivityMVP.java
+++ Source/ZeusApps.News/ZeusApps.News.Clients/Android/app/src/main/java/ua/in/zeusapps/ukrainenews/modules/main/MainActivityMVP.java
@@ -1,21 +1,14 @@
 package ua.in.zeusapps.ukrainenews.modules.main;
 
-import java.util.List;
-
-import rx.Observable;
 import ua.in.zeusapps.ukrainenews.common.BaseMVP;
-import ua.in.zeusapps.ukrainenews.models.Source;
 
 public interface MainActivityMVP {
     interface View extends BaseMVP.IView {
-        void updateSources(List<Source> sources);
     }
 
     interface Presenter extends BaseMVP.IPresenter<View> {
-
     }
 
     interface Model extends BaseMVP.IModel {
-        Observable<List<Source>> getSources();
     }
 }
