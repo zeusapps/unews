04c7d9d7d4615d6e19b9a68656785e10ef63db41
Eugen <e.zagvozdkin@gmail.com>
2017-01-27 10:44:59 +0200

    models and presenters marked as singeletons

--- Source/ZeusApps.News/ZeusApps.News.Clients/Android/app/src/main/java/ua/in/zeusapps/ukrainenews/modules/articles/ArticleModule.java
+++ Source/ZeusApps.News/ZeusApps.News.Clients/Android/app/src/main/java/ua/in/zeusapps/ukrainenews/modules/articles/ArticleModule.java
@@ -1,16 +1,20 @@
 package ua.in.zeusapps.ukrainenews.modules.articles;
 
+import javax.inject.Singleton;
+
 import dagger.Module;
 import dagger.Provides;
 
 @Module
 public class ArticleModule {
     @Provides
+    @Singleton
     public ArticleMVP.IPresenter providePresenter(ArticleMVP.IModel model){
         return new ArticlePresenter(model);
     }
 
     @Provides
+    @Singleton
     public ArticleMVP.IModel provideModel(){
         return new ArticleModel();
     }
