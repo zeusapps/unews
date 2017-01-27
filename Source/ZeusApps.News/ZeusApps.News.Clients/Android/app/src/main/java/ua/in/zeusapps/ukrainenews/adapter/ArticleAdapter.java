cdc4587a68643a68dc3fa98aadfbb6b47ef3dc25
Eugen <e.zagvozdkin@gmail.com>
2017-01-27 11:37:29 +0200

    added protocol to change selected source

--- Source/ZeusApps.News/ZeusApps.News.Clients/Android/app/src/main/java/ua/in/zeusapps/ukrainenews/adapter/ArticleAdapter.java
+++ Source/ZeusApps.News/ZeusApps.News.Clients/Android/app/src/main/java/ua/in/zeusapps/ukrainenews/adapter/ArticleAdapter.java
@@ -9,14 +9,27 @@ import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+
 import butterknife.BindView;
 import ua.in.zeusapps.ukrainenews.R;
 import ua.in.zeusapps.ukrainenews.models.Article;
 
 public class ArticleAdapter extends BaseAdapter<ArticleAdapter.ArticleHolder, Article> {
 
+    private final static String DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss'Z'";
+
+    private final SimpleDateFormat _formatterUTC;
+
     public ArticleAdapter(Activity activity) {
         super(activity);
+
+        _formatterUTC = new SimpleDateFormat(DATE_FORMAT);
+        _formatterUTC.setTimeZone(TimeZone.getTimeZone("UTC"));
     }
 
     @Override
@@ -41,7 +54,17 @@ public class ArticleAdapter extends BaseAdapter<ArticleAdapter.ArticleHolder, Ar
         public void update(Context context, Article article) {
             super.update(context, article);
 
-            publishedTextView.setText(article.getPublished());
+            String published;
+
+            try {
+                Date date = _formatterUTC.parse(article.getPublished());
+                published = SimpleDateFormat.getDateTimeInstance().format(date);
+            } catch (ParseException e) {
+                e.printStackTrace();
+                published = "";
+            }
+
+            publishedTextView.setText(published);
             titleTextView.setText(article.getTitle());
 
             String url = article.getImageUrl();
--- Source/ZeusApps.News/ZeusApps.News.Clients/Android/app/src/main/java/ua/in/zeusapps/ukrainenews/modules/main/MainActivity.java
+++ Source/ZeusApps.News/ZeusApps.News.Clients/Android/app/src/main/java/ua/in/zeusapps/ukrainenews/modules/main/MainActivity.java
@@ -10,13 +10,20 @@ import ua.in.zeusapps.ukrainenews.R;
 import ua.in.zeusapps.ukrainenews.common.BaseActivity;
 import ua.in.zeusapps.ukrainenews.common.BaseMVP;
 import ua.in.zeusapps.ukrainenews.components.ApplicationComponent;
+import ua.in.zeusapps.ukrainenews.models.Source;
 import ua.in.zeusapps.ukrainenews.modules.articles.ArticleFragment;
+import ua.in.zeusapps.ukrainenews.modules.articles.ArticleMVP;
 import ua.in.zeusapps.ukrainenews.modules.source.SourceFragment;
 
-public class MainActivity extends BaseActivity implements MainActivityMVP.View {
+public class MainActivity
+        extends BaseActivity
+        implements MainActivityMVP.View, SourceFragment.OnSelectedSourceChangedListener {
 
     @Inject
     MainActivityMVP.Presenter presenter;
+
+    @Inject
+    ArticleMVP.IPresenter articlePresenter;
 
     @Override
     protected int getContentResourceId() {
@@ -42,5 +49,12 @@ public class MainActivity extends BaseActivity implements MainActivityMVP.View {
                 .add(R.id.activity_main_sourceFragmentPlaceholder, new SourceFragment())
                 .add(R.id.activity_main_content, new ArticleFragment())
                 .commit();
+    }
+
+    @Override
+    public void onSourceChanged(Source source) {
+        if (source != null){
+            articlePresenter.updateArticles(source.getKey());
+        }
     }
 }
