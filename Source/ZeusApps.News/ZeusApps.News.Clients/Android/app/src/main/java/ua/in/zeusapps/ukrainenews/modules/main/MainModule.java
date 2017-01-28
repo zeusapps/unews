069e6eebf2a06c1bdecbf6b16f7ad16878b29d5f
Eugen <e.zagvozdkin@gmail.com>
2017-01-28 11:45:42 +0200

    updated module definition

--- Source/ZeusApps.News/ZeusApps.News.Clients/Android/app/src/main/java/ua/in/zeusapps/ukrainenews/modules/main/MainModule.java
+++ Source/ZeusApps.News/ZeusApps.News.Clients/Android/app/src/main/java/ua/in/zeusapps/ukrainenews/modules/main/MainModule.java
@@ -2,12 +2,16 @@ package ua.in.zeusapps.ukrainenews.modules.main;
 
 import dagger.Module;
 import dagger.Provides;
+import ua.in.zeusapps.ukrainenews.modules.articleView.ArticleViewMVP;
+import ua.in.zeusapps.ukrainenews.modules.articles.ArticleMVP;
 
 @Module
 public class MainModule {
 
     @Provides
-    public MainActivityMVP.Presenter provideMainActivityPresenter(){
-        return new MainActivityPresenter();
+    public MainActivityMVP.Presenter provideMainActivityPresenter(
+            ArticleMVP.IPresenter articlePresenter,
+            ArticleViewMVP.IPresenter articleViewPresenter){
+        return new MainActivityPresenter(articlePresenter, articleViewPresenter);
     }
 }
