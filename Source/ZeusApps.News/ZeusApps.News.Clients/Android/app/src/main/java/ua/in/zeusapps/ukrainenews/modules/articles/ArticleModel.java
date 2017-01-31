e42f9a4ac2d78d9ebce17cfb4e9acffb288cda8b
Eugen <e.zagvozdkin@gmail.com>
2017-01-31 16:34:45 +0200

    added new capabilities to repositry

--- Source/ZeusApps.News/ZeusApps.News.Clients/Android/app/src/main/java/ua/in/zeusapps/ukrainenews/modules/articles/ArticleModel.java
+++ Source/ZeusApps.News/ZeusApps.News.Clients/Android/app/src/main/java/ua/in/zeusapps/ukrainenews/modules/articles/ArticleModel.java
@@ -1,10 +1,8 @@
 package ua.in.zeusapps.ukrainenews.modules.articles;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import rx.Observable;
-import rx.functions.Func1;
 import ua.in.zeusapps.ukrainenews.common.BaseModel;
 import ua.in.zeusapps.ukrainenews.models.Article;
 import ua.in.zeusapps.ukrainenews.models.Source;
@@ -12,6 +10,8 @@ import ua.in.zeusapps.ukrainenews.services.IArticleService;
 import ua.in.zeusapps.ukrainenews.services.ISourceService;
 
 public class ArticleModel extends BaseModel implements ArticleMVP.IModel {
+
+    private static final int PAGE_SIZE = 20;
 
     private final IArticleService _articleService;
     private final ISourceService _sourcesService;
@@ -24,15 +24,31 @@ public class ArticleModel extends BaseModel implements ArticleMVP.IModel {
 
     @Override
     public Observable<List<Article>> getArticles(String sourceId) {
-
-        Observable<List<Article>> observable = _articleService.getArticles(sourceId);
+        Observable<List<Article>> observable = _articleService.getArticles(sourceId, PAGE_SIZE);
 
         return wrapObservable(observable);
     }
 
     @Override
+    public Observable<List<Article>> getNewerArticles(String sourceId, Article firstArticle) {
+        return getArticlePage(sourceId, firstArticle, false);
+    }
+
+    @Override
+    public Observable<List<Article>> getOlderArticles(String sourceId, Article lastArticle) {
+        return getArticlePage(sourceId, lastArticle, true);
+    }
+
+
+    @Override
     public Observable<List<Source>> getSources() {
         Observable<List<Source>> observable = _sourcesService.getSources();
+        return wrapObservable(observable);
+    }
+
+    private Observable<List<Article>> getArticlePage(String sourceId, Article article, boolean isAfter){
+        Observable<List<Article>> observable =
+                _articleService.getNewerArticles(sourceId, PAGE_SIZE, article.getPublished(), isAfter);
 
         return wrapObservable(observable);
     }
