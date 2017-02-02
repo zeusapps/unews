e290c99d8ad47169db3a4e2f323c8efa79112c66
Eugen <e.zagvozdkin@gmail.com>
2017-02-02 17:19:44 +0200

    article adapter extends ads base adapter

--- Source/ZeusApps.News/ZeusApps.News.Clients/Android/app/src/main/java/ua/in/zeusapps/ukrainenews/adapter/ArticleAdapter.java
+++ Source/ZeusApps.News/ZeusApps.News.Clients/Android/app/src/main/java/ua/in/zeusapps/ukrainenews/adapter/ArticleAdapter.java
@@ -2,47 +2,71 @@ package ua.in.zeusapps.ukrainenews.adapter;
 
 import android.app.Activity;
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdSize;
-import com.google.android.gms.ads.AdView;
 import com.squareup.picasso.Picasso;
 
 import butterknife.BindView;
-import butterknife.Optional;
 import ua.in.zeusapps.ukrainenews.R;
-import ua.in.zeusapps.ukrainenews.common.BaseFragment;
 import ua.in.zeusapps.ukrainenews.models.Article;
 import ua.in.zeusapps.ukrainenews.services.Formatter;
 
-public class ArticleAdapter extends BaseAdapter<ArticleAdapter.ArticleHolder, Article> {
+public class ArticleAdapter
+        //extends BaseAdapter<Article>{
+        extends BaseAdsAdapter<Article>{
 
     private Formatter _formatter;
 
     public ArticleAdapter(Activity activity, Formatter formatter) {
-        super(activity);
+        super(activity
+                , new AdsProvider() {
+
+
+            @Override
+            public int getAdsOffset() {
+                return 3;
+            }
+
+            @Override
+            public int getAdsPeriod() {
+                return 5;
+            }
+
+            @Override
+            public int getAdTypeAtPosition(int position) {
+                return 0;
+            }
+
+            @Override
+            public BaseViewHolder getViewHolder(LayoutInflater inflater, ViewGroup parent, int adType) {
+                View view = inflater.inflate(R.layout.fragment_article_ads_template, parent, false);
+                return new AdHolder(view);
+            }
+
+            @Override
+            public void bindAdAtPosition(BaseViewHolder holder, int position) {
+                holder.update(null, null);
+            }
+        }
+        );
 
         _formatter = formatter;
     }
 
     @Override
-    public ArticleHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view;
-
-        if (viewType == BaseAdapter.AD_TYPE){
-            view = getSimpleView(R.layout.fragment_article_ads_template, parent);
-            return new AdHolder(view);
-        }else {
-            view = getSimpleView(R.layout.fragment_article_item_template, parent);
-            return new ArticleHolder(view);
-        }
-
-
+    public ArticleHolder onCreateContentViewHolder(ViewGroup parent, int viewType) {
+        View view = getSimpleView(R.layout.fragment_article_item_template, parent);
+        return new ArticleHolder(view);
     }
+//    @Override
+//    public ArticleHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+//        View view = getSimpleView(R.layout.fragment_article_item_template, parent);
+//        return new ArticleHolder(view);
+//    }
 
     public class ArticleHolder extends BaseViewHolder<Article> {
         @BindView(R.id.fragment_article_item_template_published)
@@ -75,22 +99,6 @@ public class ArticleAdapter extends BaseAdapter<ArticleAdapter.ArticleHolder, Ar
                         .load(url)
                         .into(articleImageView);
             }
-        }
-    }
-
-    public class AdHolder extends ArticleHolder{
-
-        @BindView(R.id.fragment_article_ads_template_adView)
-        AdView adView;
-
-        public AdHolder(View itemView) {
-            super(itemView);
-        }
-
-        @Override
-        public void update(Context context, Article article) {
-            AdRequest adRequest = new AdRequest.Builder().build();
-            adView.loadAd(adRequest);
         }
     }
 }
