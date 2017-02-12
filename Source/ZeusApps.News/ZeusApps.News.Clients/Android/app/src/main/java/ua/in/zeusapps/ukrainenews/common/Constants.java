28923cd2248ba3e2a90775e58fe2ac6e6e5607b1
Eugen Zagvozdkin <e.zagvozdkin@gmail.com>
2017-02-12 21:47:10 +0200

    added remote adress

--- Source/ZeusApps.News/ZeusApps.News.Clients/Android/app/src/main/java/ua/in/zeusapps/ukrainenews/common/Constants.java
+++ Source/ZeusApps.News/ZeusApps.News.Clients/Android/app/src/main/java/ua/in/zeusapps/ukrainenews/common/Constants.java
@@ -2,7 +2,9 @@ package ua.in.zeusapps.ukrainenews.common;
 
 public class Constants {
 
-    public static final String REMOTE_URL = "http://10.0.3.2:8080";
+    public static final String REMOTE_URL =
+            //"http://10.0.3.2:8080";
+            "http://158.69.207.43";
 
     private Constants(){
 
