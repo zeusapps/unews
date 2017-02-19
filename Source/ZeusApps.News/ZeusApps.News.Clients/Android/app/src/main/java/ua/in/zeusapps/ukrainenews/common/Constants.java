2432e74c54339de6f82084e79043aa042d386ec5
Eugen Zagvozdkin <e.zagvozdkin@gmail.com>
2017-02-19 19:19:05 +0200

    changed uri to domain

--- Source/ZeusApps.News/ZeusApps.News.Clients/Android/app/src/main/java/ua/in/zeusapps/ukrainenews/common/Constants.java
+++ Source/ZeusApps.News/ZeusApps.News.Clients/Android/app/src/main/java/ua/in/zeusapps/ukrainenews/common/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
 
     public static final String REMOTE_URL =
             //"http://10.0.3.2:8080";
-            "http://158.69.207.43";
+            "http://zeusapps.in.ua";
 
     private Constants(){
 
