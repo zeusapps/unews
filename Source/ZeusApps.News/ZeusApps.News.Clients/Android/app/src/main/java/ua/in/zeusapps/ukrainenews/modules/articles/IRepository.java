82eb3fa4491fc89d845ee5fd3fb53dd0df765821
Eugen <eugen@git.innernet>
2017-02-24 18:10:00 +0200

    added get article by id

--- Source/ZeusApps.News/ZeusApps.News.Clients/Android/app/src/main/java/ua/in/zeusapps/ukrainenews/modules/articles/IRepository.java
+++ Source/ZeusApps.News/ZeusApps.News.Clients/Android/app/src/main/java/ua/in/zeusapps/ukrainenews/modules/articles/IRepository.java
@@ -14,6 +14,7 @@ public interface IRepository {
 
     List<Article> getArticlesPage(String sourceId, Article fromArticle, int count);
 
+    Article getArticle(String articleId);
     void deleteAlArticles();
 
     void addAllSources(List<Source> sources);
