bfe4a4c4e787418562618974a5714847afbe2219
Eugen Zagvozdkin <e.zagvozdkin@gmail.com>
2017-04-14 22:48:03 +0300

    added getting remote sources from server to ensure interator, simplified splash screen presenter

--- Source/ZeusApps.News/ZeusApps.News.Clients/Android/app/src/main/java/ua/in/zeusapps/ukrainenews/domain/EnsureSourcesInteractor.java
+++ Source/ZeusApps.News/ZeusApps.News.Clients/Android/app/src/main/java/ua/in/zeusapps/ukrainenews/domain/EnsureSourcesInteractor.java
@@ -1,25 +1,54 @@
 package ua.in.zeusapps.ukrainenews.domain;
 
+import android.support.annotation.NonNull;
+
 import java.util.List;
 
 import javax.inject.Inject;
 
 import rx.Observable;
+import rx.functions.Func1;
 import ua.in.zeusapps.ukrainenews.common.Interactor;
 import ua.in.zeusapps.ukrainenews.models.Source;
+import ua.in.zeusapps.ukrainenews.services.IDataService;
 import ua.in.zeusapps.ukrainenews.services.ISourceRepository;
 
 public class EnsureSourcesInteractor extends Interactor<Boolean, List<Source>> {
 
-    private ISourceRepository _repository;
+    private final ISourceRepository _repository;
+    private final IDataService _dataService;
 
     @Inject
-    public EnsureSourcesInteractor(ISourceRepository repository) {
+    public EnsureSourcesInteractor(
+            ISourceRepository repository,
+            IDataService dataService) {
         _repository = repository;
+        _dataService = dataService;
     }
 
     @Override
     protected Observable<Boolean> buildObservable(List<Source> remoteSources) {
+        return _dataService.getSources()
+                .onErrorResumeNext(new Func1<Throwable, Observable<? extends List<Source>>>() {
+                    @Override
+                    public Observable<? extends List<Source>> call(Throwable throwable) {
+                        return Observable.just(_repository.getAll());
+                    }
+                })
+                .map(new Func1<List<Source>, Boolean>() {
+                    @Override
+                    public Boolean call(List<Source> sources) {
+                        return checkSources(sources);
+                    }
+                });
+    }
+
+    @NonNull
+    private Boolean checkSources(List<Source> remoteSources){
+        if (remoteSources == null || remoteSources.size() == 0){
+            return false;
+        }
+
 
         List<Source> localSources = _repository.getAll();
         for (Source local: localSources) {
@@ -34,6 +63,6 @@ public class EnsureSourcesInteractor extends Interactor<Boolean, List<Source>> {
             }
         }
 
-        return Observable.just(true);
+        return true;
     }
 }
--- Source/ZeusApps.News/ZeusApps.News.Clients/Android/app/src/main/java/ua/in/zeusapps/ukrainenews/modules/splash/SplashPresenter.java
+++ Source/ZeusApps.News/ZeusApps.News.Clients/Android/app/src/main/java/ua/in/zeusapps/ukrainenews/modules/splash/SplashPresenter.java
@@ -4,21 +4,15 @@ import android.util.Log;
 
 import com.arellomobile.mvp.InjectViewState;
 
-import java.util.List;
-
 import javax.inject.Inject;
 
 import rx.Subscriber;
 import ua.in.zeusapps.ukrainenews.common.MvpPresenterBase;
 import ua.in.zeusapps.ukrainenews.domain.EnsureSourcesInteractor;
-import ua.in.zeusapps.ukrainenews.domain.GetSourcesInteractor;
-import ua.in.zeusapps.ukrainenews.models.Source;
 
 @InjectViewState
 public class SplashPresenter extends MvpPresenterBase<SplashView> {
 
-    @Inject
-    GetSourcesInteractor sourcesInteractor;
     @Inject
     EnsureSourcesInteractor ensureSourcesInteractor;
 
@@ -29,11 +23,9 @@ public class SplashPresenter extends MvpPresenterBase<SplashView> {
 
     @Override
     protected void onFirstViewAttach() {
-        // TODO concat getting new sources and check for existence
-        sourcesInteractor.execute(new Subscriber<List<Source>>() {
+        ensureSourcesInteractor.execute(new Subscriber<Boolean>() {
             @Override
             public void onCompleted() {
-
             }
 
             @Override
@@ -42,22 +34,8 @@ public class SplashPresenter extends MvpPresenterBase<SplashView> {
             }
 
             @Override
-            public void onNext(List<Source> sources) {
-                ensureSourcesInteractor.execute(sources, new Subscriber<Boolean>() {
-                    @Override
-                    public void onCompleted() {
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-                        showError(e);
-                    }
-
-                    @Override
-                    public void onNext(Boolean aBoolean) {
-                        getViewState().startApp();
-                    }
-                });
+            public void onNext(Boolean aBoolean) {
+                getViewState().startApp();
             }
         });
     }
