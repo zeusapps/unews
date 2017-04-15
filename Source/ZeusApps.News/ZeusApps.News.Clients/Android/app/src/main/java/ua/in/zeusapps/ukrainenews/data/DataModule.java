ac634e42bdbdce2f682dcb6834083b867bf5b3ee
Eugen Zagvozdkin <e.zagvozdkin@gmail.com>
2017-04-15 19:44:45 +0300

    added ads provider

--- Source/ZeusApps.News/ZeusApps.News.Clients/Android/app/src/main/java/ua/in/zeusapps/ukrainenews/data/DataModule.java
+++ Source/ZeusApps.News/ZeusApps.News.Clients/Android/app/src/main/java/ua/in/zeusapps/ukrainenews/data/DataModule.java
@@ -6,19 +6,27 @@ import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
+import ua.in.zeusapps.ukrainenews.adapter.RecyclerViewAdapter;
+import ua.in.zeusapps.ukrainenews.modules.articles.AdsProvider;
 
 @Module
 public class DataModule {
 
     @Provides
     @Singleton
-    public ISourceRepository providesSourcesRepository(Context context){
+    ISourceRepository providesSourcesRepository(Context context){
         return new SourceRepository(context);
     }
 
     @Provides
     @Singleton
-    public IArticleRepository providesArticlesRepository(Context context){
+    IArticleRepository providesArticlesRepository(Context context){
         return new ArticleRepository(context);
+    }
+
+    @Provides
+    @Singleton
+    RecyclerViewAdapter.AdsProvider providesAdsProvider(Context context){
+        return new AdsProvider(context);
     }
 }
