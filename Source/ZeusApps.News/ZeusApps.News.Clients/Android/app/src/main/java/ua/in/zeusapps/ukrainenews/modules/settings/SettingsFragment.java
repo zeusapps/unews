e570aeda9c0ebe26c0deed80fc8634aaecf26ce9
Eugen Zagvozdkin <e.zagvozdkin@gmail.com>
2017-04-19 10:42:40 +0300

    code cleanup

--- Source/ZeusApps.News/ZeusApps.News.Clients/Android/app/src/main/java/ua/in/zeusapps/ukrainenews/modules/settings/SettingsFragment.java
+++ Source/ZeusApps.News/ZeusApps.News.Clients/Android/app/src/main/java/ua/in/zeusapps/ukrainenews/modules/settings/SettingsFragment.java
@@ -1,7 +1,5 @@
 package ua.in.zeusapps.ukrainenews.modules.settings;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
