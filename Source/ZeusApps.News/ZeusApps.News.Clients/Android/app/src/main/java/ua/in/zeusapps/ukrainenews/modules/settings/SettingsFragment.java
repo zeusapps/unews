3f2ebdae41e849f504e9ef7d77111d359f0b3dbf
Eugen Zagvozdkin <e.zagvozdkin@gmail.com>
2017-04-19 14:59:36 +0300

    updated settings stuff, removed unused files, added registration for components

--- Source/ZeusApps.News/ZeusApps.News.Clients/Android/app/src/main/java/ua/in/zeusapps/ukrainenews/modules/settings/SettingsFragment.java
+++ Source/ZeusApps.News/ZeusApps.News.Clients/Android/app/src/main/java/ua/in/zeusapps/ukrainenews/modules/settings/SettingsFragment.java
@@ -1,80 +1,51 @@
 package ua.in.zeusapps.ukrainenews.modules.settings;
 
-import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v7.app.ActionBar;
-import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
 import android.view.View;
 
-import javax.inject.Inject;
+import com.arellomobile.mvp.presenter.InjectPresenter;
 
-import butterknife.BindView;
 import butterknife.OnClick;
 import ua.in.zeusapps.ukrainenews.R;
-import ua.in.zeusapps.ukrainenews.common.BaseFragment;
-import ua.in.zeusapps.ukrainenews.components.ApplicationComponent;
+import ua.in.zeusapps.ukrainenews.common.Layout;
+import ua.in.zeusapps.ukrainenews.modules.root.BaseRootFragment;
 
+@Layout(R.layout.fragment_settings)
 public class SettingsFragment
-        extends BaseFragment
-        implements SettingsMVP.View{
+        extends BaseRootFragment
+        implements SettingsView {
 
-    public static final String TAG = SettingsFragment.class.getSimpleName();
-
-    @BindView(R.id.include_toolbar)
-    Toolbar toolbar;
-    @Inject
-    SettingsMVP.Presenter presenter;
+    @InjectPresenter
+    SettingsPresenter presenter;
 
     @OnClick(R.id.activity_settings_clearButton)
     public void onClearStorage(){
         getPresenter().clearStorage();
     }
 
-    @Override
-    protected int getContentResourceId() {
-        return R.layout.fragment_settings;
-    }
-
-    @Override
-    public String GetTag() {
-        return TAG;
-    }
-
-    @Override
-    protected void onCreateViewOverride(View view, @Nullable Bundle savedInstanceState) {
-        setSupportActionBar();
-        setHasOptionsMenu(true);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home){
-            getCompatActivity().onBackPressed();
-            return true;
-        }
-        return false;
-    }
-
-    @Override
-    protected void inject(ApplicationComponent component) {
-        component.inject(this);
-    }
-
     @NonNull
     @Override
-    protected SettingsMVP.Presenter getPresenter() {
+    public SettingsPresenter getPresenter() {
         return presenter;
     }
 
-    private void setSupportActionBar(){
-        toolbar.setTitle(getString(R.string.settings));
-        getCompatActivity().setSupportActionBar(toolbar);
-        ActionBar actionBar = getCompatActivity().getSupportActionBar();
-        if (actionBar != null)
-        {
-            actionBar.setDisplayHomeAsUpEnabled(true);
-        }
+    @Override
+    public void articlesCleared() {
+        showInfo(getString(R.string.fragment_settings_articles_cleared));
+    }
+
+    @Override
+    public String getTitle() {
+        return getString(R.string.fragment_settings_title);
+    }
+
+    @Override
+    public int getFabButtonIcon() {
+        return 0;
+    }
+
+    @Override
+    public View.OnClickListener getFabButtonAction() {
+        return null;
     }
 }
