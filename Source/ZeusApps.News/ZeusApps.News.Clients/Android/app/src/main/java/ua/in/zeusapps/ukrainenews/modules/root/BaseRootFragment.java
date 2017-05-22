1d3e26c2f9fd5e3a4a1e78c340a70a16f9009467
Eugen Zagvozdkin <e.zagvozdkin@gmail.com>
2017-05-22 21:27:24 +0300

    added lifecircle event

--- Source/ZeusApps.News/ZeusApps.News.Clients/Android/app/src/main/java/ua/in/zeusapps/ukrainenews/modules/root/BaseRootFragment.java
+++ Source/ZeusApps.News/ZeusApps.News.Clients/Android/app/src/main/java/ua/in/zeusapps/ukrainenews/modules/root/BaseRootFragment.java
@@ -3,6 +3,7 @@ package ua.in.zeusapps.ukrainenews.modules.root;
 import android.os.Bundle;
 import android.support.annotation.DrawableRes;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.View;
 
 import ua.in.zeusapps.ukrainenews.common.MvpFragment;
@@ -17,7 +18,7 @@ public abstract class BaseRootFragment
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        //setRetainInstance(true);
+        setRetainInstance(true);
         RootActivity activity = (RootActivity) getActivity();
         activity.resolveToolbar(this);
         activity.resolveFab(this);
@@ -42,5 +43,61 @@ public abstract class BaseRootFragment
 
     public RootActivity getRootActivity(){
         return (RootActivity) getActivity();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        String message = savedInstanceState == null
+                ? "null"
+                : String.valueOf(savedInstanceState.size());
+        Log.i(getClass().getSimpleName(), "onCreate: " + message);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        Log.i(getClass().getSimpleName(), "onStart()");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        Log.i(getClass().getSimpleName(), "onResume()");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        String message = outState == null
+                ? "null"
+                : String.valueOf(outState.size());
+
+        Log.i(getClass().getSimpleName(), "onSaveInstanceState(): " + message);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        Log.i(getClass().getSimpleName(), "onStop()");
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+
+        Log.i(getClass().getSimpleName(), "onDestroyView()");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        Log.i(getClass().getSimpleName(), "onDestroy()");
     }
 }
