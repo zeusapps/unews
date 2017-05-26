a2977f441ffc3e96733f569b2bd80fd6e73f9d45
Eugen Zagvozdkin <e.zagvozdkin@gmail.com>
2017-05-26 14:48:57 +0300

    removed main actvity from app component

--- Source/ZeusApps.News/ZeusApps.News.Clients/Android/app/src/main/java/ua/in/zeusapps/ukrainenews/components/ApplicationComponent.java
+++ Source/ZeusApps.News/ZeusApps.News.Clients/Android/app/src/main/java/ua/in/zeusapps/ukrainenews/components/ApplicationComponent.java
@@ -11,8 +11,6 @@ import ua.in.zeusapps.ukrainenews.modules.articles.ArticleFragment;
 import ua.in.zeusapps.ukrainenews.modules.articles.ArticlePresenter;
 import ua.in.zeusapps.ukrainenews.modules.articlesDetails.ArticleViewPresenter;
 import ua.in.zeusapps.ukrainenews.modules.articlesDetails.ArticlesDetailsPresenter;
-import ua.in.zeusapps.ukrainenews.modules.main.MainActivity;
-import ua.in.zeusapps.ukrainenews.modules.main.MainModule;
 import ua.in.zeusapps.ukrainenews.modules.root.RootPresenter;
 import ua.in.zeusapps.ukrainenews.modules.settings.SettingsPresenter;
 import ua.in.zeusapps.ukrainenews.modules.sources.SourcesPresenter;
@@ -21,14 +19,11 @@ import ua.in.zeusapps.ukrainenews.modules.splash.SplashPresenter;
 @Singleton
 @Component(modules = {
         ApplicationModule.class,
-        MainModule.class,
         DataModule.class
 })
 public interface ApplicationComponent {
-    void inject(MainActivity target);
     void inject(ArticleFragment fragment);
     void inject(ArticleViewFragment fragment);
-
     void inject(SourcesPresenter presenter);
     void inject(SplashPresenter presenter);
     void inject(ArticlePresenter presenter);
