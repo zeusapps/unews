8a5c20ce7e4f6e92ac384c96dcf87dbc5f0f0237
Eugen Zagvozdkin <e.zagvozdkin@gmail.com>
2017-06-20 22:27:29 +0300

    registered fabric

--- Source/ZeusApps.News/ZeusApps.News.Clients/Android/app/src/main/java/ua/in/zeusapps/ukrainenews/common/App.java
+++ Source/ZeusApps.News/ZeusApps.News.Clients/Android/app/src/main/java/ua/in/zeusapps/ukrainenews/common/App.java
@@ -2,10 +2,12 @@ package ua.in.zeusapps.ukrainenews.common;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
 import com.google.android.gms.ads.MobileAds;
 import com.twitter.sdk.android.core.Twitter;
 
 import butterknife.ButterKnife;
+import io.fabric.sdk.android.Fabric;
 import ua.in.zeusapps.ukrainenews.R;
 import ua.in.zeusapps.ukrainenews.components.DaggerApplicationComponent;
 import ua.in.zeusapps.ukrainenews.components.ApplicationComponent;
@@ -35,6 +37,7 @@ public class App extends Application {
 
         MobileAds.initialize(getApplicationContext(), getString(R.string.AD_APP_ID));
         Twitter.initialize(this);
+        Fabric.with(this, new Crashlytics());
 
         _instance = this;
     }
