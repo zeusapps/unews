23e031bb01f297d2cfca689786688c67f72413f2
Andrew UI <andrew.ui@git.innernet>
2017-07-06 15:21:56 +0300

    updated application component

--- Source/ZeusApps.News/ZeusApps.News.Clients/Android/app/src/main/java/ua/in/zeusapps/ukrainenews/components/ApplicationComponent.java
+++ Source/ZeusApps.News/ZeusApps.News.Clients/Android/app/src/main/java/ua/in/zeusapps/ukrainenews/components/ApplicationComponent.java
@@ -3,15 +3,16 @@ package ua.in.zeusapps.ukrainenews.components;
 import javax.inject.Singleton;
 
 import dagger.Component;
+import ua.in.zeusapps.ukrainenews.components.articles.ArticlesActivity;
+import ua.in.zeusapps.ukrainenews.components.articles.ArticlesPresenter;
 import ua.in.zeusapps.ukrainenews.components.details.DetailsPresenter;
 import ua.in.zeusapps.ukrainenews.components.details.fragments.ArticleDetailsFragment;
 import ua.in.zeusapps.ukrainenews.components.details.fragments.ArticleDetailsPresenter;
+import ua.in.zeusapps.ukrainenews.components.main.fragments.pager.PagerPresenter;
 import ua.in.zeusapps.ukrainenews.components.main.fragments.topStories.TopStoriesFragment;
 import ua.in.zeusapps.ukrainenews.components.main.fragments.topStories.TopStoriesPresenter;
 import ua.in.zeusapps.ukrainenews.data.DataModule;
 import ua.in.zeusapps.ukrainenews.modules.ApplicationModule;
-import ua.in.zeusapps.ukrainenews.components.main.fragments.articles.ArticleFragment;
-import ua.in.zeusapps.ukrainenews.components.main.fragments.articles.ArticlePresenter;
 import ua.in.zeusapps.ukrainenews.components.main.MainPresenter;
 import ua.in.zeusapps.ukrainenews.components.main.fragments.settings.SettingsPresenter;
 import ua.in.zeusapps.ukrainenews.components.main.fragments.sources.SourcesPresenter;
@@ -24,10 +25,8 @@ import ua.in.zeusapps.ukrainenews.components.splash.SplashPresenter;
         DataModule.class
 })
 public interface ApplicationComponent {
-    void inject(ArticleFragment fragment);
     void inject(SourcesPresenter presenter);
     void inject(SplashPresenter presenter);
-    void inject(ArticlePresenter presenter);
     void inject(SettingsPresenter presenter);
     void inject(MainPresenter presenter);
     void inject(DetailsPresenter presenter);
@@ -35,4 +34,7 @@ public interface ApplicationComponent {
     void inject(ArticleDetailsFragment fragment);
     void inject(TopStoriesFragment fragment);
     void inject(TopStoriesPresenter presenter);
+    void inject(PagerPresenter presenter);
+    void inject(ArticlesPresenter presenter);
+    void inject(ArticlesActivity activity);
 }
